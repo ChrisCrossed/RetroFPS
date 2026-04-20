233a2acbe72e0db2f1e4bf9d15482c1903b8768e
ChrisCrossed <chriscrossed.86@gmail.com>
2026-04-20 00:34:22 -0600

    Starting Enemy Pathing Logic

--- Assets/Scripts/Enemies/ENEMY_OBJ.cs
+++ Assets/Scripts/Enemies/ENEMY_OBJ.cs
@@ -1,19 +1,28 @@
 using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ENEMY_OBJ : MonoBehaviour
 {
     private protected int Health;
     [SerializeField, Range(50, 250)] private protected int HEALTH_MAX = 100;
+    [SerializeField] protected Transform[] NavigationLocations;
+
+    NavMeshAgent ThisNavMeshAgent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private protected virtual void Start()
     {
         START_Connections();
+
+        if(NavigationLocations.Length > 0)
+            ThisNavMeshAgent.SetDestination(NavigationLocations[0].position);
     }
 
     private protected virtual void START_Connections()
     {
+        ThisNavMeshAgent = GetComponent<NavMeshAgent>();
+
         Health = HEALTH_MAX;
     }
 
---
+++ Assets/Scripts/c_NavMeshPositionLogic.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class c_NavMeshPositionLogic : MonoBehaviour
+{
+    // Awake is called before Start
+    void Awake()
+    {
+        // this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
